a5786519daab3e21f3b80fcb1192f14fbc269cd1
d- <d-@users.noreply.github.com>
2024-06-13 14:36:35 +0000

    redirect

--- api/index.py
+++ api/index.py
@@ -1,11 +1,11 @@
-from flask import Flask
-
+from flask import Flask, redirect
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
-    
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    new_path = 'https://dwood.io/' + path
+    return redirect(new_path, code=302)
+
+if __name__ == '__main__':
+    app.run()
